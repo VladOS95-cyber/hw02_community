555735853edef832797d330a2d1c72781f411b1d
Vladislav <vladislav.bronzov@gmail.com>
2020-11-28 12:35:50 +0300

    First version

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+
 from django.contrib.auth import get_user_model
 
 
@@ -6,8 +8,38 @@ User = get_user_model()
 
 
 class Post(models.Model):
+    """Модель публикации, содержащая
+    текст к публикации, дату, имя автора и
+    ссылку на модель Group
+    """
+
     text = models.TextField()
     pub_date = models.DateField("date published", auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, 
-    related_name="posts"
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, 
+        related_name="posts"
     )
+    group = models.ForeignKey(
+        "Group", on_delete=models.CASCADE, blank=True, null=True
+    )
+
+
+class Group(models.Model):
+    """Модель сообщества куда буду попадать публикации
+    в зависимости от тематики. Блогер будет иметь возможность
+    самостоятельно выбрать группу, но создать группу
+    смогут только админы. Модель имеет свойства:
+    title(имя), адрес(slug) и описание(description)
+    """
+
+    title = models.CharField()
+    slug = models.SlugField()
+    description = models.TextField()
+
+    def __str__(self):
+        """Метод для печати поля title"""
+
+        return self.title
+
+
+
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,19 @@
-from django.http import HttpResponse
-from .models import Post
-from django.shortcuts import render
+from .models import Post, Group
+
+
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
     latest = Post.objects.order_by('-pub_date')[:11]
     # собираем тексты постов в один, разделяя новой строкой
     return render(request, "index.html", {"posts": latest})
+
+def group_posts(request, slug):
+    """Функция возвращает страницу сообщества
+    и выводит до 12 записей на странице 
+    """
+
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
+    return render(request, "group.html", {"group": group, "posts": posts})
